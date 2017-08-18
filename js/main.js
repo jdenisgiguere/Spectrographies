ec9a96f9a6df7303af9893642d18337c1a0fb165
Jean-Denis Giguere <jean-denis@anagraph.io>
2017-08-18 12:24:35 -0400

    slider refactoring

--- js/main.js
+++ js/main.js
@@ -16,6 +16,8 @@ var map = L.Mapzen.map('map', {
       ] } }
 });*/
 
+
+
 var map = L.map('map', {
           zoomControl: true}).setView([45.5314, -73.6750], 9);
 
@@ -94,138 +96,100 @@ marker = new L.geoJSON(markers, {
 
   onEachFeature: function(feature, layer) {
 
+     var baseURL = "https://web.fulcrumapp.com/shares/3a4bbd0435c58166/photos/"
 
-var baseURL = "https://web.fulcrumapp.com/shares/3a4bbd0435c58166/photos/"
+    // Fonction qui récupère le UUID de chaque photos
 
-// Fonction qui récupère le UUID de chaque photos
-
-
-var x 
-
-// Capture le pop up //
-map.on('popupopen', function (e) {
-                 
-      var h2 = document.getElementById('titrePopUp');
-      var img = document.getElementById('myImg');
-      var modal = document.getElementById('myModal');
-      var captionText = document.getElementById("caption");
-      var carouselDiv = document.getElementById("carousel-inner")
-
-    // Fonction pour creer le contenu du modal //
-img.onclick = function(){ 
-           map.closePopup();
-           modal.style.display = "block";
-           captionText.innerHTML = img.alt
-           //carouselDiv.innerHTML = photoCount()
-           var x= (e.popup._source.feature.properties.photos_url)
-           var j
-          function photos_uuid(j){
-              j = x
-              for(var i = 0; i < j.length; i++) {
-            return x.substring(70).split('%2C')}
-            }
-
-          function test(){for(var i = 0; i < divsFromPhotosUrl(x).length ; i++) {
-           return carouselDiv.innerHTML = divsFromPhotosUrl(photos_uuid(x))
-         }}
- 
-          test()
-
-
-
-
-var k
-
-function divsFromPhotosUrl(k) {
- var photos = photos_uuid(k)
- console.log(photos_uuid(k))
- var divsAsText = '';
- var currentDiv;
-  for(var i = 0; i < photos.length ; i++) {
-    currentDiv = '<div class="item active"><img src="'+baseURL+photos[i]+'" id="img0'+i+'" ></div>';
-   // divsAsText = divsAsText + currentDiv;
-   console.log(currentDiv)
-     return currentDiv;
-   }}
-
-
-
-
-
-
-
-    // Fonction pour fermer le modal //
-      var span = document.getElementsByClassName("close")[0];
-      span.onclick = function() { 
-      modal.style.display = "none";
-      }
-    }
-});
 
 ///////// START Bind tout le data dans le pop up /////////
+    var popupContent = '<div id="popUpOpen"><a href="#" id="titrePopUp"><h2>'
 
-     layer.bindPopup(
-     '<div id="popUpOpen"><a href="#" id="titrePopUp"><h2>'
+      // Pop Up Content
 
-     // Pop Up Content
-
-       +feature.properties.titre
-       +'</h2></a><h3 style="display:inline">'
-       +feature.properties.genre
-       +' & '
-       +feature.properties.type
-       +'</h3><h4> Par : '
-       +feature.properties.pseudo
+      +feature.properties.titre
+      +'</h2></a><h3 style="display:inline">'
+      +feature.properties.genre
+      +' & '
+      +feature.properties.type
+      +'</h3><h4> Par : '
+      +feature.properties.pseudo
 
 
       // Modal Section Content //
 
 
-         +'</h4><div id=&quot;attributeContainer&quot;>'
-         // Image section
-         +'<img class="img-thumbnail "id="myImg"'
-         // Alt section with all the data for the modal popup
-                 +' alt="<div><h2>'
-                 +feature.properties.titre
-                 +'</h2><br><h3 style=&quot;display: inline !important;&quot;> Genre : </h3><h4 style=&quot;display: inline !important;&quot;>'
-                 +feature.properties.genre
-                 +'</h4><br><br><h3 style=&quot;display: inline !important;&quot;> Type : </h3><h4 style=&quot;display: inline !important;&quot;>'
-                 +feature.properties.type
-                 +'</h4><br><h3> Contexte : </h3><p>'
-                 +feature.properties.description_contexte
-                 +'</p><div class=&quot;well&quot; style=&quot;margin-top:15px !important&quot;><div class=&quot;col-sm-8 well &quot; ><h3 style=&quot;display: inline !important;&quot;> Auteur : </h3><h4 style=&quot;display: inline !important;&quot;>'
-                 +feature.properties.pseudo
-                  // Autoportrait //
-                 +'</h4></div><div class=&quot;col-sm-4 well&quot;><img  id=&quot;selfie&quot; class=&quot;img-thumbnail&quot; src=&quot;'
-                 +feature.properties.autoportrait_url.replace('view?photos=','')
-                 +'&quot;</img></div><div class=&quot;col well&quot;><h3> Relation au lieu : </h3><p>'
-                 +feature.properties.relation_au_lieu_appartenance
-                 +'</p></div></div></div>'
-                 // Audio //
-                 +'<div class=&quot;col  well&quot;>'
-                 +'<audio controls controls controlsList=&quot;nodownload&quot;><source src=&quot;'
-                 +feature.properties.audio_url.replace('view?audio=','')
-                 //+feature.properties.audio_url.replace('view?audio=','')
-                 +'&quot;</audio></div></div></div>'
-                 // End of alt attribution
-                 // Caroussel
-         +'" src="'
-         //+photosPreURL+first
-         +feature.properties.photos_url.substring(0,106).replace('view?photos=','') 
-         +'"></div></img>'   
-        
-    // Socials // 
+      +'</h4><div id=&quot;attributeContainer&quot;>'
+      // Image section
+      +'<img class="img-thumbnail "id="myImg"'
+      // Alt section with all the data for the modal popup
+      +' alt="<div><h2>'
+      +feature.properties.titre
+      +'</h2><br><h3 style=&quot;display: inline !important;&quot;> Genre : </h3><h4 style=&quot;display: inline !important;&quot;>'
+      +feature.properties.genre
+      +'</h4><br><br><h3 style=&quot;display: inline !important;&quot;> Type : </h3><h4 style=&quot;display: inline !important;&quot;>'
+      +feature.properties.type
+      +'</h4><br><h3> Contexte : </h3><p>'
+      +feature.properties.description_contexte
+      +'</p><div class=&quot;well&quot; style=&quot;margin-top:15px !important&quot;><div class=&quot;col-sm-8 well &quot; ><h3 style=&quot;display: inline !important;&quot;> Auteur : </h3><h4 style=&quot;display: inline !important;&quot;>'
+      +feature.properties.pseudo
+      // Autoportrait //
+      +'</h4></div><div class=&quot;col-sm-4 well&quot;><img  id=&quot;selfie&quot; class=&quot;img-thumbnail&quot; src=&quot;'
+      +feature.properties.autoportrait_url.replace('view?photos=','')
+      +'&quot;</img></div><div class=&quot;col well&quot;><h3> Relation au lieu : </h3><p>'
+      +feature.properties.relation_au_lieu_appartenance
+      +'</p></div></div></div>'
+      // Audio //
+      +'<div class=&quot;col  well&quot;>'
+      +'<audio controls controls controlsList=&quot;nodownload&quot;><source src=&quot;'
+      +feature.properties.audio_url.replace('view?audio=','')
+      //+feature.properties.audio_url.replace('view?audio=','')
+      +'&quot;</audio></div></div></div>'
+      // End of alt attribution
+      // Caroussel
+      +'" src="'
+      //+photosPreURL+first
+      +feature.properties.photos_url.substring(0,106).replace('view?photos=','')
+      +'"></div></img>'
+
+      // Socials //
 
       //Facbook
-     +'<iframe src="https://www.facebook.com/plugins/share_button.php?href='
-       + feature.properties.photos_url.replace('view?photos=','')
-       +'&layout=button&size=small&mobile_iframe=false&width=59&height=20&appId" width="59" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>'
+      +'<iframe src="https://www.facebook.com/plugins/share_button.php?href='
+      + feature.properties.photos_url.replace('view?photos=','')
+      +'&layout=button&size=small&mobile_iframe=false&width=59&height=20&appId" width="59" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>'
 
-       +'  '
-      //Twitter 
-       +'<iframe id="tweet-button" allowtransparency="true" frameborder="0" scrolling="no"  src="http://platform.twitter.com/widgets/tweet_button.html?url='
-       +feature.properties.photos_url.replace('view?photos=','')+
-       '&amp;count=horizontal"   style="width:110px; height:20px;"></iframe>')
+      +'  '
+      //Twitter
+      +'<iframe id="tweet-button" allowtransparency="true" frameborder="0" scrolling="no"  src="http://platform.twitter.com/widgets/tweet_button.html?url='
+      +feature.properties.photos_url.replace('view?photos=','')+
+      '&amp;count=horizontal"   style="width:110px; height:20px;"></iframe></div>'
+
+     layer.bindPopup(popupContent) .on('popupopen', function (e) {
+
+       var h2 = document.getElementById('titrePopUp');
+       var img = document.getElementById('myImg');
+       var modal = document.getElementById('myModal');
+       var captionText = document.getElementById("caption");
+       var carouselDiv = document.getElementById("carousel-inner");
+
+       // Fonction pour creer le contenu du modal //
+       img.onclick = function () {
+         map.closePopup();
+         modal.style.display = "block";
+         captionText.innerHTML = img.alt
+         var feature_photos_url = e.popup._source.feature.properties.photos_url;
+
+         var feature_photos_uuid = photos_uuid(feature_photos_url);
+
+         carouselDiv.innerHTML = divsFromPhotosUrl(feature_photos_url);
+
+         // Fonction pour fermer le modal //
+         $(".close").bind("click", function(e) {
+           $('#myModal').css('display', 'none') ;
+         });
+
+       };
+     });
 
     
     } ///////// END Bind tout le data dans le pop up /////////
@@ -400,68 +364,11 @@ console.log(value)
 
   onEachFeature: function(feature, layer) {
 
-    
-var baseURL = "https://web.fulcrumapp.com/shares/3a4bbd0435c58166/photos/"
-
 // Fonction qui récupère le UUID de chaque photos
 
 
-var x 
-
-// Capture le pop up //
-map.on('popupopen', function (e) {
-                 
-      var h2 = document.getElementById('titrePopUp');
-      var img = document.getElementById('myImg');
-      var modal = document.getElementById('myModal');
-      var captionText = document.getElementById("caption");
-      var carouselDiv = document.getElementById("carousel-inner")
-
-    // Fonction pour creer le contenu du modal //
-img.onclick = function(){ 
-           map.closePopup();
-           modal.style.display = "block";
-           captionText.innerHTML = img.alt
-           //carouselDiv.innerHTML = photoCount()
-           var x= (e.popup._source.feature.properties.photos_url)
-           var j
-          function photos_uuid(j){
-              j = x
-              for(var i = 0; i < j.length; i++) {
-            return x.substring(70).split('%2C')}
-            }
-
-          function test(){for(var i = 0; i < divsFromPhotosUrl(x).length ; i++) {
-           return carouselDiv.innerHTML = divsFromPhotosUrl(photos_uuid(x))
-         }}
- 
-          test()
 
 
-
-
-var k
-
-function divsFromPhotosUrl(k) {
- var photos = photos_uuid(k)
- console.log(photos_uuid(k))
- var divsAsText = '';
- var currentDiv;
-  for(var i = 0; i < photos.length ; i++) {
-    currentDiv = '<div class="item active"><img src="'+baseURL+photos[i]+'" id="img0'+i+'" ></div>';
-   // divsAsText = divsAsText + currentDiv;
-   console.log(currentDiv)
-     return currentDiv;
-   }}
-
-
-    // Fonction pour fermer le modal //
-      var span = document.getElementsByClassName("close")[0];
-      span.onclick = function() { 
-      modal.style.display = "none";
-      }
-    }
-});
 
 ///////// START Bind tout le data dans le pop up /////////
 
@@ -526,16 +433,16 @@ function divsFromPhotosUrl(k) {
       //Twitter 
        +'<iframe id="tweet-button" allowtransparency="true" frameborder="0" scrolling="no"  src="http://platform.twitter.com/widgets/tweet_button.html?url='
        +feature.properties.photos_url.replace('view?photos=','')+
-       '&amp;count=horizontal"   style="width:110px; height:20px;"></iframe>')
+       '&amp;count=horizontal"   style="width:110px; height:20px;"></iframe>'); ///////// END Bind tout le data dans le pop up /////////
 
     
-    } ///////// END Bind tout le data dans le pop up /////////
+    } // End of onEachFeature
 
 
 
 
 
-  })
+  }); // End of L.GeoJSON instantiating filteredMarker;
 
 //////////////////////
 
@@ -624,4 +531,57 @@ $("#titreBox").on("select2:unselect", function(e) {
 
 });
 
-})
+});
+
+
+/**
+ * Get a search parameter by its name
+ * https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+ *
+ * @param name :  string Name of the parameters
+ * @param url : string URL to parse
+ * @returns {*}
+ */
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+/**
+ * Array of the photos associated to the photos url
+ *
+ * @param photos_url
+ * @returns {Array|*}
+ */
+function photos_uuid(photos_url) {
+  return getParameterByName('photos', photos_url).split(',');
+}
+
+/**
+ * Create a string representing the div for every photos from a photo url
+ * @param photos_url
+ * @returns {string}
+ */
+function divsFromPhotosUrl(photos_url) {
+  var baseURL = "https://web.fulcrumapp.com/shares/3a4bbd0435c58166/photos/"
+  var photos = photos_uuid(photos_url);
+  var divsAsText = '';
+  var currentDiv;
+  for (var i = 0; i < photos.length; i++) {
+    if (i == 0) {
+      currentDiv = '<div class="item active"><img src="' + baseURL + photos[i] + '" id="img' + i + '" ></div>';
+    } else {
+      currentDiv = '<div class="item"><img src="' + baseURL + photos[i] + '" id="img' + i + '" ></div>';
+    }
+
+    divsAsText = divsAsText + currentDiv;
+  }
+
+  return divsAsText;
+}
+
